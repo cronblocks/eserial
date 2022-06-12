fd3344dad6e349d9208879e43d42176fbce1a322
usama <mahmoodify@gmail.com>
2022-06-12 20:03:39 +0500

    Dispatching port discovery items

--- Src/ESerial.SerialLib/SerialService.cs
+++ Src/ESerial.SerialLib/SerialService.cs
@@ -7,11 +7,26 @@ namespace ESerial.SerialLib
 {
     public class SerialService
     {
+        public event Action<string> NewPortFound;
+        public event Action<string> PortRemoved;
+
         private PortsDiscoverer _portDiscoverer;
         
         public SerialService()
         {
             _portDiscoverer = new PortsDiscoverer();
+            _portDiscoverer.NewPortFound += OnNewPortFound;
+            _portDiscoverer.PortRemoved += OnPortRemoved;
+        }
+
+        private void OnPortRemoved(string port)
+        {
+            PortRemoved?.Invoke(port);
+        }
+
+        private void OnNewPortFound(string port)
+        {
+            NewPortFound?.Invoke(port);
         }
 
         public void StartService()
