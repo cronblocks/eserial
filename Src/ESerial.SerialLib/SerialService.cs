2ad2b8071d14c6fca81a0aa5b90d546d16b5ea99
usama <mahmoodify@gmail.com>
2022-06-12 18:26:15 +0500

    Adding port finder

--- Src/ESerial.SerialLib/SerialService.cs
+++ Src/ESerial.SerialLib/SerialService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESerial.SerialLib.Internals;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,23 @@ namespace ESerial.SerialLib
 {
     public class SerialService
     {
+        private PortFinder _portFinder;
+        private List<string> _portNamesList;
+
+        public SerialService()
+        {
+            _portFinder = new PortFinder();
+            _portNamesList = new List<string>();
+        }
+
         public void StartService()
         {
-
+            _portNamesList.Clear();
         }
 
         public void StopService()
         {
-
+            _portNamesList.Clear();
         }
     }
 }
