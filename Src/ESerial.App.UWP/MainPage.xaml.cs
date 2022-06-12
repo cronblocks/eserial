c2201c33c80fe711d13d3a0f376b0e47c4c62229
usama <mahmoodify@gmail.com>
2022-06-12 20:28:33 +0500

    Port items update added

--- Src/ESerial.App.UWP/MainPage.xaml.cs
+++ Src/ESerial.App.UWP/MainPage.xaml.cs
@@ -5,8 +5,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -37,11 +39,33 @@ namespace ESerial.App.UWP
         private void OnNewSerialPortFound(string port)
         {
             Debug.WriteLine($"Port Added: {port}");
+
+            _ = SerialPortComboBox.Dispatcher.TryRunAsync(
+                    CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+                        SerialPortComboBox.Items.Add(port);
+                        if (SerialPortComboBox.Items.Count == 1)
+                        {
+                            SerialPortComboBox.SelectedIndex = 0;
+                        }
+                    });
         }
 
         private void OnSerialPortRemoved(string port)
         {
             Debug.WriteLine($"Port Removed: {port}");
+
+            _ = SerialPortComboBox.Dispatcher.TryRunAsync(
+                    CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+                        SerialPortComboBox.Items.Remove(port);
+                        if (SerialPortComboBox.Items.Count == 0)
+                        {
+                            SerialPortComboBox.SelectedIndex = -1;
+                        }
+                    });
         }
 
         private void OnGuiLoaded(object sender, RoutedEventArgs e)
