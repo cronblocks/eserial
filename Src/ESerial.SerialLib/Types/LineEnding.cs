c6cbcdb1f0a16682f19c102d7e76583b6d70915d
usama <mahmoodify@gmail.com>
2022-06-13 14:10:45 +0500

    Adding line ending enum

---
+++ Src/ESerial.SerialLib/Types/LineEnding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESerial.SerialLib.Types
+{
+    public enum LineEnding
+    {
+        None,
+        CR,
+        LF,
+        CRLF
+    }
+}
