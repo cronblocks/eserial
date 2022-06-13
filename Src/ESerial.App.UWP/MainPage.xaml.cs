5d0a5e6e912d19271145661a304fd75540264b57
usama <mahmoodify@gmail.com>
2022-06-13 18:35:35 +0500

    Updating summary

--- Src/ESerial.App.UWP/MainPage.xaml.cs
+++ Src/ESerial.App.UWP/MainPage.xaml.cs
@@ -11,7 +11,7 @@ using Windows.UI.Xaml.Controls;
 namespace ESerial.App.UWP
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Page backend that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
