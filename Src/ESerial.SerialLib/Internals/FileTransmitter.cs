44619c08faa2b2222628caaf68bf8d7761326a82
usama <mahmoodify@gmail.com>
2022-06-14 16:08:43 +0500

    Adding fields and preparing outline

--- Src/ESerial.SerialLib/Internals/FileTransmitter.cs
+++ Src/ESerial.SerialLib/Internals/FileTransmitter.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace ESerial.SerialLib.Internals
 {
     internal class FileTransmitter
     {
+        private readonly string _filename;
+        private readonly PortCommunicator _portCommunicator;
+
+        private bool _isRunning;
+        private FileStream _fileStream;
+        private Thread _thread;
+
+        public FileTransmitter(string filename, PortCommunicator portCommunicator)
+        {
+            _filename = filename;
+            _portCommunicator = portCommunicator;
+
+            _fileStream = new FileStream(_filename, FileMode.Open, FileAccess.Read);
+        }
+
+        public void StartTransmission()
+        {
+
+        }
+
+        public void QuitTransmission()
+        {
+
+        }
     }
 }
