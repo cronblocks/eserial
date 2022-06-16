1c57ffbf662dd2d52ae544675028f8f82d8fd671
usama <mahmoodify@gmail.com>
2022-06-16 19:28:10 +0500

    Implementing the settings provision

--- Src/ESerial.App.WPF/Settings/SettingsProvider.cs
+++ Src/ESerial.App.WPF/Settings/SettingsProvider.cs
@@ -11,15 +11,79 @@ namespace ESerial.App.WPF.Settings
     internal class SettingsProvider : ISettingsProvider
     {
         private readonly string SETTINGS_DIRNAME;
-        private const string SETTINGS_FILENAME = "Settings.ini";
+        private readonly string SETTINGS_FILENAME;
 
-        public SettingsProvider()
+        private Dictionary<string, string> SETTINGS;
+
+        public SettingsProvider(string settingsFilename = "Settings.ini")
         {
+            SETTINGS_FILENAME = settingsFilename;
             SETTINGS_DIRNAME = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}ESerial";
 
             if (!Directory.Exists(SETTINGS_DIRNAME))
             {
                 Directory.CreateDirectory(SETTINGS_DIRNAME);
+            }
+
+            SETTINGS = new Dictionary<string, string>();
+
+            ReadSettingsFile();
+        }
+
+        public void SetString(string key, string value)
+        {
+            SETTINGS[key] = value;
+        }
+
+        public string GetString(string key)
+        {
+            if (SETTINGS.ContainsKey(key))
+            {
+                return SETTINGS[key];
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void SaveSettings()
+        {
+            try
+            {
+                using (StreamWriter file = new StreamWriter(Path.Combine(SETTINGS_DIRNAME, SETTINGS_FILENAME)))
+                {
+                    foreach (KeyValuePair<string, string> setting in SETTINGS)
+                    {
+                        file.WriteLine($"{setting.Key} = {setting.Value}");
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void ReadSettingsFile()
+        {
+            if (File.Exists(Path.Combine(SETTINGS_DIRNAME, SETTINGS_FILENAME)))
+            {
+                try
+                {
+                    using (StreamReader file = new StreamReader(Path.Combine(SETTINGS_DIRNAME, SETTINGS_FILENAME)))
+                    {
+                        string? line;
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            line = line.Trim();
+
+                            string[] parts = line.Split('=');
+                            if (parts.Length == 2)
+                            {
+                                SETTINGS[parts[0].Trim()] = parts[1].Trim();
+                            }
+                        }
+                    }
+                }
+                catch (Exception) { }
             }
         }
     }
