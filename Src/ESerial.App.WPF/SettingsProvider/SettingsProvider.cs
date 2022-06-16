c5eaa58a93fa2a0d8652c75e5e9836aa52f9891e
usama <mahmoodify@gmail.com>
2022-06-16 18:45:29 +0500

    Adding blank implementation for settings provider interface

---
+++ Src/ESerial.App.WPF/SettingsProvider/SettingsProvider.cs
@@ -0,0 +1,13 @@
+﻿using ESerial.SerialLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESerial.App.WPF.SettingsProvider
+{
+    internal class SettingsProvider : ISettingsProvider
+    {
+    }
+}
