ea09ef83577f670eb619eaa3f019e496c84b7bde
usama <mahmoodify@gmail.com>
2022-06-16 19:00:04 +0500

    Adding store and retrieve iface funcs

--- Src/ESerial.SerialLib/ISettingsProvider.cs
+++ Src/ESerial.SerialLib/ISettingsProvider.cs
@@ -6,5 +6,7 @@ namespace ESerial.SerialLib
 {
     public interface ISettingsProvider
     {
+        void StoreString(string key, string value);
+        string RetrieveString(string key);
     }
 }
